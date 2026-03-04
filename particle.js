a7b17bd49714ca5dbdbfd55c9125f53ac3190ba5
lololololoklk3049 <alekare73@gmail.com>
2026-03-03 21:14:48 -0500

    Add particle.js configuration for particle effects

---
+++ particle.js
@@ -0,0 +1,85 @@
+particlesJS('particles-js', {
+    particles: {
+        number: {
+            value: 80,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        },
+        color: {
+            value: ['#ff0000', '#cc0000', '#990000']
+        },
+        shape: {
+            type: 'circle'
+        },
+        opacity: {
+            value: 0.7,
+            random: true,
+            anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.3,
+                sync: false
+            }
+        },
+        size: {
+            value: 3,
+            random: true,
+            anim: {
+                enable: true,
+                speed: 2,
+                size_min: 0.5,
+                sync: false
+            }
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: '#ff0000',
+            opacity: 0.3,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 2,
+            direction: 'none',
+            random: true,
+            straight: false,
+            out_mode: 'out',
+            bounce: false,
+            attract: {
+                enable: true,
+                rotateX: 600,
+                rotateY: 1200
+            }
+        }
+    },
+    interactivity: {
+        detect_on: 'canvas',
+        events: {
+            onhover: {
+                enable: true,
+                mode: 'grab'
+            },
+            onclick: {
+                enable: true,
+                mode: 'push'
+            },
+            resize: true
+        },
+        modes: {
+            grab: {
+                distance: 200,
+                line_linked: {
+                    opacity: 0.8,
+                    color: '#ff0000'
+                }
+            },
+            push: {
+                particles_nb: 3
+            }
+        }
+    },
+    retina_detect: true
+});
